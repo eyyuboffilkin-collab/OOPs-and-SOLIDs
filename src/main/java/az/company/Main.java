22a73687944df6c0cb5daea27902a4093bb65be3
Ilkin Eyyubov <eyyuboffilkin@example.com>
2026-03-20 18:05:06 +0400

    OOPs in Java

---
+++ src/main/java/az/company/Main.java
@@ -0,0 +1,12 @@
+package az.company;
+
+import java.util.List;
+
+public class Main {
+    static void main(String[] args) {
+        Student student = new Student("Ilkin", "Eyyubov", 21);
+
+        student.printinfo();
+    }
+
+}
